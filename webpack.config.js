2ba0d0e427a789af364dbe1033dd67b7c77237eb
Vlad-Buko <vladislav_buko@mail.ru>
2022-11-18 23:53:28 +0300

    version next

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
-var path = require('path');
-
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    mode: 'development',
+            entry: "./src/main/js/src/App.js",
     output: {
         path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        filename: './src/main/resources/static/built/main.js'
     },
-    module: {
-        rules: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }]
-            }
-        ]
-    }
-};
+            module: {
+                rules: [
+                    {
+                        test: /\.jsx?$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader",
+                        },
+                    },
+                    {
+                        test: /\.css$/i,
+                        use: ["style-loader", "css-loader"],
+                    },
+                ],
+            },
+            optimization: {
+                minimize: true,
+            },
+
+            performance: {
+                hints: false,
+                maxEntrypointSize: 512000,
+                maxAssetSize: 512000
+            },
+            resolve: {
+                modules: ['node_modules'],
+                extensions: ['', '.js', '.jsx', '.css'],
+            },
+        };
